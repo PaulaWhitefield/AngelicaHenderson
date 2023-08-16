a1662855f77e61195ac4dcc2a187716a260709e1
PaulaWhitefield <paula.whitefield@themailbox.io>
2023-08-16 21:34:49 +0100

    Updated email address and anti-harvest protection.

--- src/contact.php
+++ src/contact.php
@@ -6,10 +6,13 @@ layout: src/_includes/base.php
 ---
 
 <p class="w3-large">I love to read your messages, so feel free to send in an email anytime.</p>
-<p id="email_address"><span class="w3-deep-purple w3-padding w3-round-large">contact [at] angelicahenderson [dot] ict [dot] rocks</span></p>
+<p id="email_address"><span class="w3-deep-purple w3-padding w3-round-large">angelica [dot] henderson [at] themailbox [dot] io</span></p>
 <script type="text/javascript">
     var email_address = document.getElementById( "email_address" );
+    var email_address_span = email_address.getElementsByTagName( "span" )[0];
     
-    email_address.innerHTML = '<a href="mailto:contact' + '@' + 'angelicahenderson' + '.' + 'ict' + '.' + 'rocks" target="_blank" class="w3-deep-purple w3-hover-pink w3-button w3-round-large"><b>contact' + '@' + 'angelicahenderson' + '.' + 'ict' + '.' + 'rocks</b></a>';
+    var the_email_address = email_address_span.innerHTML.replaceAll( ' [at] ', '@' ).replaceAll( ' [dot] ', '.' );
+    
+    email_address.innerHTML = '<a href="mailto:' + the_email_address + '" target="_blank" class="w3-deep-purple w3-hover-pink w3-button w3-round-large"><b>' + the_email_address + '</b></a>';
 </script>
 
