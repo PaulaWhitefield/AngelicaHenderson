c4331ceabac284b438fcc3409af16e1bdafdf57f
PaulaWhitefield <paula.whitefield@themailbox.io>
2023-10-17 22:22:21 +0100

    Moved static assets relative to site + other tweaks

--- src/_includes/base.php
+++ src/_includes/base.php
@@ -1,7 +1,7 @@
 ---
 content_placeholder: {{ content }}
 storage-domain: storage.dah5.com
-images-url: https://images.storage.dah5.com
+images-url: /assets/images
 ---
 <!DOCTYPE html>
 <html lang="en" style="min-height: 100vh;<?php if( isset( $metadata['background_image'] ) && $metadata['background_image'] ): ?> background-image: url('<?php echo $metadata['images-url'] . '/backgrounds' . $metadata['background_image']; ?>'); background-size: cover; background-repeat: no-repeat;<?php endif; ?>">
@@ -11,12 +11,12 @@ images-url: https://images.storage.dah5.com
         
         <title><?php if( isset( $metadata['page_title'] ) && $metadata['page_title'] ) echo $metadata['page_title'] . " - "; ?>Angelica Henderson</title>
         
-        <link rel="stylesheet" type="text/css" href="https://w3css.--- metadata.storage-domain ---/4.15/w3.css">
-        <link rel="stylesheet" type="text/css" href="https://webfonts.--- metadata.storage-domain ---/nunito/nunito.css">
+        <link rel="stylesheet" type="text/css" href="/assets/w3css/4.15/w3.css">
+        <link rel="stylesheet" type="text/css" href="/assets/webfonts/nunito/nunito.css">
     </head>
     
     <body class="font-nunito<?php if( isset( $metadata['body_class'] ) && $metadata['body_class'] ) echo ' ' . $metadata['body_class']; ?>">
-        <div class="w3-content w3-section w3-round-large w3-padding-large w3-padding-24 w3-text-light-gray" style="background-color: rgba( 0, 0, 0, 0.5 );">
+        <div class="w3-content w3-section w3-round-large w3-padding-large w3-padding-24 w3-text-light-gray" style="background-color: rgba( 0, 0, 0, 0.6 );">
             <h1 class="font-nunito w3-center w3-text-pink w3-xxxlarge"><b class="w3-padding w3-bottombar w3-border-deep-purple">Angelica Henderson</b></h1>
             <p class="w3-center w3-text-white w3-xlarge"><b>It's Time To Relax!</b></p>
             
@@ -29,6 +29,8 @@ images-url: https://images.storage.dah5.com
             <div class="w3-padding">
                 {{ content }}
             </div>
+
+            <p class="w3-center w3-text-white w3-small">Copyright &copy; Angelica Henderson.<br>Site by <a href="https://www.paulawhitefield.com" target="_blank">Paula Whitefield</a>.</p>
         </div>
     </body>
 </html>
